13285670bed2665ccb5855be06a84ab54deb49a9
Alexander Ljungberg <aljungberg@wireload.net>
2011-11-07 17:23:56 +0000

    Include bin soft links in Cappuccino releases.

--- install_cappuccino_base.sh
+++ install_cappuccino_base.sh
@@ -34,7 +34,18 @@ fi
 echo "Install /usr/local/cappuccino in $HERE?"
 echo "WARNING: all files not supposed to be in the folder will be deleted."
 if prompt "no"; then
-    rsync -acvP --exclude .git --exclude README.md --exclude install_cappuccino_base.sh --exclude README.markdown --exclude zips/\* --delete /usr/local/cappuccino/ "$HERE"
+    rsync -acvP --exclude-from install_cappuccino_exclude.rsync --delete /usr/local/cappuccino/ "$HERE"
+
+    # Make links relative.
+
+    for link in "cappuccino/bin/capp" "cappuccino/bin/cib-dump" "objective-j/bin/cplutil" "cappuccino/bin/flatten" "jake/bin/jake" "narwhal-jsc/bin/narwhal-jsc" "cappuccino/bin/nib2cib" "objective-j/bin/objj" "objective-j/bin/objjc" "OJTest/bin/ojautotest" "OJTest/bin/ojautotest-run" "cappuccino/bin/ojdepcheck" "OJTest/bin/ojtest" "cappuccino/bin/press" "shrinksafe/bin/shrinksafe" "narwhal-jsc/bin/source-test.sh"
+    do
+        filename=`basename "$link"`
+        (cd bin && ln -sf "../packages/$link" "$filename");
+    done
+    (cd bin && ln -sf narwhal js)
+    (cd bin && ln -sf activate.bash activate)
+
     echo "Version size: `du -sh \"$HERE\"`"
 
     echo "Commit the new build?"
@@ -42,6 +53,8 @@ if prompt "no"; then
         git add .
         git add --force packages/*
         git add --force packages/narwhal-jsc/deps/*
+        git add --force bin/*
+
         git commit
     fi
 fi
