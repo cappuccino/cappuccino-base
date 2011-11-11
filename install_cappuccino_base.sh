d0377973e0aec118a1e12dbb210f87bd1af0b683
Alexander Ljungberg <aljungberg@wireload.net>
2011-11-11 17:50:08 +0000

    Remove JSC binaries from base distribution.

--- install_cappuccino_base.sh
+++ install_cappuccino_base.sh
@@ -37,8 +37,14 @@ echo "WARNING: all files not supposed to be in the folder will be deleted."
 if prompt "no"; then
     rsync -acvP --exclude-from install_cappuccino_exclude.rsync --delete "$cappuccino_source/" "$destination"
 
-    # Make links relative.
+    # Don't include JSC binaries. They should be built on the target machine by bootstrap.
+    # \.(o|dylib|a|la|lo|lai) were already excluded by the rsync exclude.
+    for jsc_bin in "packages/narwhal-jsc/bin/narwhal-webkit" "packages/narwhal-jsc/deps/libedit-20100424-3.0/config.h"  "packages/narwhal-jsc/deps/libedit-20100424-3.0/config.log" "packages/narwhal-jsc/deps/libedit-20100424-3.0/config.status" "packages/narwhal-jsc/deps/libedit-20100424-3.0/doc/editline.3" "packages/narwhal-jsc/deps/libedit-20100424-3.0/doc/editrc.5" "packages/narwhal-jsc/deps/libedit-20100424-3.0/doc/Makefile" "packages/narwhal-jsc/deps/libedit-20100424-3.0/examples/.libs/fileman" "packages/narwhal-jsc/deps/libedit-20100424-3.0/examples/.libs/tc1" "packages/narwhal-jsc/deps/libedit-20100424-3.0/examples/fileman" "packages/narwhal-jsc/deps/libedit-20100424-3.0/examples/Makefile" "packages/narwhal-jsc/deps/libedit-20100424-3.0/examples/tc1" "packages/narwhal-jsc/deps/libedit-20100424-3.0/libedit.pc" "packages/narwhal-jsc/deps/libedit-20100424-3.0/libtool" "packages/narwhal-jsc/deps/libedit-20100424-3.0/Makefile" "packages/narwhal-jsc/deps/libedit-20100424-3.0/src/.libs/libedit.0.dylib.dSYM/Contents/Info.plist" "packages/narwhal-jsc/deps/libedit-20100424-3.0/src/common.h" "packages/narwhal-jsc/deps/libedit-20100424-3.0/src/emacs.h" "packages/narwhal-jsc/deps/libedit-20100424-3.0/src/fcns.c" "packages/narwhal-jsc/deps/libedit-20100424-3.0/src/fcns.h" "packages/narwhal-jsc/deps/libedit-20100424-3.0/src/help.c" "packages/narwhal-jsc/deps/libedit-20100424-3.0/src/help.h" "packages/narwhal-jsc/deps/libedit-20100424-3.0/src/Makefile" "packages/narwhal-jsc/deps/libedit-20100424-3.0/src/vi.h" "packages/narwhal-jsc/deps/libedit-20100424-3.0/stamp-h1" "packages/narwhal-jsc/include/config.h" "packages/narwhal-jsc/narwhal-jsc.conf"
+    do
+        rm "$jsc_bin";
+    done
 
+    # Make links relative.
     for link in "cappuccino/bin/capp" "cappuccino/bin/cib-dump" "objective-j/bin/cplutil" "cappuccino/bin/flatten" "jake/bin/jake" "narwhal-jsc/bin/narwhal-jsc" "cappuccino/bin/nib2cib" "objective-j/bin/objj" "objective-j/bin/objjc" "OJTest/bin/ojautotest" "OJTest/bin/ojautotest-run" "cappuccino/bin/ojdepcheck" "OJTest/bin/ojtest" "cappuccino/bin/press" "shrinksafe/bin/shrinksafe" "narwhal-jsc/bin/source-test.sh" "cappuccino/bin/fontinfo" "cappuccino/bin/xcodecapp"
     do
         filename=`basename "$link"`
